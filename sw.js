dd2ac3b7e3393741a0a0c7b692ef15f9aa05fba9
josemendes85 <jmsjr85@gmail.com>
2025-07-21 15:23:56 -0300

    ocupacao

--- sw.js
+++ sw.js
@@ -1,27 +1,24 @@
-const CACHE_NAME = 'vistorias-cache-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/sw.js',
-  '/exigencias.js',
-  'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css',
-  'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css',
-  'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js'
-];
-
-self.addEventListener('install', event => {
+self.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
+    caches.open('v1').then(function(cache) {
+      return cache.addAll([
+        './',
+        './index.html',
+        './processo.html',
+        './manifest.json',
+        './sw.js',
+        './icons/icon_192x192.png',
+        './icons/icon_512x512.png'
+      ]);
     })
   );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', function(event) {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request)
+      .then(function(response) {
+        return response || fetch(event.request);
+      })
   );
 });
