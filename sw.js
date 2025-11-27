21fed9a95639ae2ff8b276c6266ab235ee6fb2ab
josemendes85 <jmsjr85@gmail.com>
2025-11-27 15:44:58 -0300

    versao

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // 1. ATUALIZE A VERSÃO DO CACHE A CADA ALTERAÇÃO DE ARQUIVO
 // Mude esta string a cada vez que você fizer uma alteração nos arquivos cacheados.
-const CACHE_NAME = 'v7';
+const CACHE_NAME = 'v8';
 
 // Lista completa de arquivos para pré-cache, incluindo suas dependências
 const urlsToCache = [
